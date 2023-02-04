0979746103e53c83e39e8659f4e4b3fd2e70e834
lunaticsy <lunatic.yi@gmail.com>
2023-02-05 02:13:34 +0900

    for iOS

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -21,9 +21,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "0592dfe3b97f6e3fd0019861b69d1ee6",
 "icons/Icon-maskable-192.png": "8a4cfb33d7601f7799771549f89ad5f1",
 "icons/Icon-maskable-512.png": "0592dfe3b97f6e3fd0019861b69d1ee6",
-"index.html": "eee32a34893efd8f20936296feb482d2",
-"/": "eee32a34893efd8f20936296feb482d2",
-"main.dart.js": "da8daa81c19b6fe9e70df8f500bec06b",
+"index.html": "1402dd5430010460175d6a6dfc646975",
+"/": "1402dd5430010460175d6a6dfc646975",
+"main.dart.js": "125c32523b35f0e59e237bc2e2e0b4a6",
 "manifest.json": "af1c1026f4bcac4e352c1835b200e232",
 "version.json": "341e4548d2b0a88dd7832474c1935e10"
 };
